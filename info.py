800b5dd7cbe8e449b9fab6ca45f67415ed62436c
N33VARP <118649748+N33VARPV2@users.noreply.github.com>
2023-10-10 12:32:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,8 +28,8 @@ SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/499d0ed233f043fe277
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5383602320').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '5097048997').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001903522896').split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '5383602320').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '').split()]
 auth_channel = environ.get('AUTH_CHANNEL', "-1001859563032")
